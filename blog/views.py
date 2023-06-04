83e6fc9bbe3498d7789b8c49fd8ab54aae442f73
sunwooiljoon <wnsdl1659@naver.com>
2023-06-04 18:57:59 +0900

    fix : reset to basic

--- blog/views.py
+++ blog/views.py
@@ -38,6 +38,3 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
-def practice_2(request):
-    return render(request, 'blog/main.html')
